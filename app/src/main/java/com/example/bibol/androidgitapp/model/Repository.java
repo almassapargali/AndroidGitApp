0f3dacecb78bde1ef4f4792fd560020c89be8974
Bibol <bolat.bibol@gmail.com>
2015-03-12 17:00:01 +0300

    add repo

---
+++ app/src/main/java/com/example/bibol/androidgitapp/model/Repository.java
@@ -0,0 +1,45 @@
+package com.example.bibol.androidgitapp.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.Serializable;
+
+
+public class Repository implements Serializable {
+    private String name, fullName;
+    private Integer id;
+    private Integer stargazersCount,watchersCount,forksCount;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public static Repository fromJSON(JSONObject json) throws InstantiationException {
+        Repository repository = new Repository();
+        try {
+            repository.name = json.getString("name");
+            repository.fullName = json.getString("full_name");
+            repository.description = json.getString("description");
+            repository.id = json.getInt("id");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new InstantiationException("Can't parse User from given JSON");
+        }
+        return repository;
+    }
+
+}
